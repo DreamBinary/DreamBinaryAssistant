0d8a42497db3068085d11107e3cd78864d71a6c3
cxq <1647715359@qq.com>
2024-01-11 19:14:30 +0800

    update

--- start.py
+++ start.py
@@ -1,6 +1,4 @@
 import os
 
-os.system("openxlab model download --model-repo 'fiv/DreamBinaryAssistant_Merge' --output=model")
-os.system('git clone https://github.com/InternLM/InternLM.git && '
-          'cp app.py InternLM/ && '
-          'streamlit run InternLM/app.py --server.address=0.0.0.0 --server.port 7860')
+# run run.sh
+os.system("sh run.sh")
